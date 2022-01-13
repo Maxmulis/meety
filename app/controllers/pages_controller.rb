82cf2f7b67a4dc0d6ed74a0beea25b7dfa012895
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-01-13 20:39:58 +0100

    Create first geoapify http request
    GEOAPIFY

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
+require 'uri'
+require 'net/http'
+
 class PagesController < ApplicationController
   def index
-    @r = request.ip
+    api_key_string = Rails.application.credentials.geoapify
+    uri = URI("http://api.geoapify.com/v2/places?categories=commercial.supermarket&apiKey=#{api_key_string}")
+    res = Net::HTTP.get_response(uri)
+
+    return unless res.is_a?(Net::HTTPSuccess)
+
+    @res_JSON = JSON.parse(res)
+
+    puts 'JSON?::'
+    puts @res_JSON
   end
 end
