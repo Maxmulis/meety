7236cd4e814d91f71c60c7df1cf71852c34e6cc6
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-01-15 23:20:19 +0100

    edited credentials and fixed http request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,22 @@
 require 'net/http'
-
 class PagesController < ApplicationController
+  rescue_from Net::HTTPBadRequest, with: :bad_request
   def index
-    cat = 'commercial.supermarket'
-    # filters = 'circle:-87.770231,41.878968,5000'
-    filters = "circle:#{params['long']},#{params['lat']},#{params['range']}"
-    api_key_string = Rails.application.credentials.development.geoapify
-
+    cat = 'catering.restaurant'
+    lat = 52.5068959
+    long = 13.3892484 #rate mal wo das ist? :D
+    range = 1000
+    filters = "circle:#{long},#{lat},#{range}&limit=2" # filters = 'circle:-87.770231,41.878968,5000'
+    api_key_string = Rails.application.credentials.geoapify
     uri = URI("https://api.geoapify.com/v2/places?categories=#{cat}&filter=#{filters}&apiKey=#{api_key_string}")
     res = Net::HTTP.get_response(uri)
-
-    return unless res.is_a?(Net::HTTPSuccess)
-
     json = JSON.parse(res.body)
-    @res_JSON = JSON.pretty_generate(json)
+    render json: json, status: :ok
+  end
+
+  private
+
+  def bad_request(error)
+    render json: error.body, status: :bad_request
   end
 end
