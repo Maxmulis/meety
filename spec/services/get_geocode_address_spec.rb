d8e3aa886cc4aa24077d7213ff95b2e5468b7ef4
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-02-09 21:37:19 +0100

    add test that does not work

---
+++ spec/services/get_geocode_address_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe GetGeocodeAddress, type: :services do
+  subject { described_class }
+
+  it { is_expected.to be_a(Class) }
+
+  describe '#call' do
+    let(:input) { "Rudi-Dutschke-Str. 26, Berlin" }
+
+    context 'when called with a valid address returns hash with lon and lat' do
+      it {
+        is_expected.target.call(input).to be({ lon: 13.3913749,
+                                               lat: 52.506872 })
+      }
+    end
+  end
+end
