d95f17754ccbb84f7033628edefb9592234ec31c
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-02-11 09:04:49 +0100

    add test for invalid address

--- spec/services/get_geocode_address_spec.rb
+++ spec/services/get_geocode_address_spec.rb
@@ -6,12 +6,21 @@ RSpec.describe GetGeocodeAddress, type: :services do
   it { is_expected.to be_a(Class) }
 
   describe '#call' do
-    let(:input) { "Rudi-Dutschke-Str. 26, Berlin" }
-
     context 'when called with a valid address returns hash with lon and lat' do
+      let(:input) { "Rudi-Dutschke-Str. 26, Berlin" }
+
       it {
         expect(is_expected.target.call(input)).to eq({ lon: 13.3913749,
                                                        lat: 52.506872 })
+      }
+    end
+
+    context 'when called with an invalid address still returns a valid lon and lat' do
+      let(:input) { "Rudi-Dutschke-Str, Berlin" }
+
+      it {
+        expect(is_expected.target.call(input)).to have_key(:lon)
+        expect(is_expected.target.call(input)).to have_key(:lat)
       }
     end
   end
