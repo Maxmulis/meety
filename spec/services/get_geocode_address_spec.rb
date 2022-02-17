ad166560462e691bce2cfba65abc19f446afe0b1
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-02-17 18:43:47 +0100

    refactor, add raise

--- spec/services/get_geocode_address_spec.rb
+++ spec/services/get_geocode_address_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe GetGeocodeAddress, type: :services do
-  subject { described_class }
-
-  it { is_expected.to be_a(Class) }
-
   describe '#call' do
-    context 'when called with a valid address returns hash with lon and lat' do
-      let(:input) { "Rudi-Dutschke-Str. 26, Berlin" }
-
-      it {
-        expect(is_expected.target.call(input)).to eq({ lon: 13.3913749,
-                                                       lat: 52.506872 })
-      }
+    context 'when called with a valid address' do
+      # subject { GetGeocodeAddress.new("Rudi-Dutschke-Str. 26, Berlin").call }
+      # it { is_expected.to eq({ lon: 13.3913749, lat: 52.506872 }) }
+      let(:valid_address) do
+        GetGeocodeAddress.call("Rudi-Dutschke-Str. 26, Berlin")
+      end
+      it "returns a hash with the correct lon and lat" do
+        expect(valid_address).to eq({ lon: 13.3913749, lat: 52.506872 })
+      end
     end
 
-    context 'when called with an invalid address still returns a valid lon and lat' do
-      let(:input) { "Rudi-Dutschke-Str, Berlin" }
-
-      it {
-        expect(is_expected.target.call(input)).to have_key(:lon)
-        expect(is_expected.target.call(input)).to have_key(:lat)
-      }
+    context 'when called with an unspecific address' do
+      let(:unspecific_address) do
+        GetGeocodeAddress.call("Rudi-Dutschke-Str, Berlin")
+      end
+      it "raises an exception" do
+        expect { unspecific_address }.to raise_error(/unspecific address/)
+      end
+    end
+    context 'when called with an invalid address' do
+      let(:invalid_address) { GetGeocodeAddress.call("") }
+      it "raises an exception" do
+        expect { invalid_address }.to raise_error(/invalid address/)
+      end
     end
   end
 end
