de4d30274fa7806ad37d6ad820c01e27181f3e22
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-02-17 18:43:47 +0100

    refactor, add raise

--- app/services/get_geocode_address.rb
+++ app/services/get_geocode_address.rb
@@ -8,6 +8,9 @@ class GetGeocodeAddress < ApplicationService
   end
 
   def call
+    raise StandardError, "invalid address" if @address.empty?
+    raise StandardError, "unspecific address" unless @address.match?(/\d/)
+
     uri = URI("https://api.geoapify.com/v1/geocode/search?text=#{@address}&filter=countrycode:de&apiKey=#{API_KEY}")
     res = Net::HTTP.get_response(uri)
     response = JSON.parse(res.body)
