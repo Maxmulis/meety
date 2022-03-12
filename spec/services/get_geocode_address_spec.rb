729898880f179f7d80e903b3ed4d6ff885e2f39e
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-03-12 12:11:27 +0100

    delete unnecessary specs

--- spec/services/get_geocode_address_spec.rb
+++ spec/services/get_geocode_address_spec.rb
@@ -7,22 +7,9 @@ RSpec.describe GetGeocodeAddress, type: :services do
         GetGeocodeAddress.new("Rudi-Dutschke-Str. 26, Berlin").call
       end
       it "returns a hash with the correct lon and lat" do
-        expect(valid_address).to eq(lon: 13.3913749, lat: 52.506872)
-      end
-    end
-
-    context 'when called with an unspecific address' do
-      let(:unspecific_address) do
-        GetGeocodeAddress.new("Rudi-Dutschke-Str, Berlin").call
-      end
-      it 'raises an exception' do
-        expect { unspecific_address }.to raise_error(/unspecific address/)
-      end
-    end
-    context 'when called with an invalid address' do
-      let(:invalid_address) { GetGeocodeAddress.new("").call }
-      it "raises an exception" do
-        expect { invalid_address }.to raise_error(/invalid address/)
+        expect(lon: valid_address.lon,
+               lat: valid_address.lat).to eq(lon: 13.3913749,
+                                             lat: 52.506872)
       end
     end
   end
