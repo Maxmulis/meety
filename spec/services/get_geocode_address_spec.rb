cf919634de700858b6dc4d4ff5997fd78e8d617e
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-03-12 01:27:54 +0100

    refactored and extended tests

--- spec/services/get_geocode_address_spec.rb
+++ spec/services/get_geocode_address_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe GetGeocodeAddress, type: :services do
   describe '#call' do
     context 'when called with a valid address' do
-      # subject { GetGeocodeAddress.new("Rudi-Dutschke-Str. 26, Berlin").call }
-      # it { is_expected.to eq({ lon: 13.3913749, lat: 52.506872 }) }
       let(:valid_address) do
-        GetGeocodeAddress.call("Rudi-Dutschke-Str. 26, Berlin")
+        GetGeocodeAddress.new("Rudi-Dutschke-Str. 26, Berlin").call
       end
       it "returns a hash with the correct lon and lat" do
         expect(valid_address).to eq({ lon: 13.3913749, lat: 52.506872 })
@@ -15,14 +13,14 @@ RSpec.describe GetGeocodeAddress, type: :services do
 
     context 'when called with an unspecific address' do
       let(:unspecific_address) do
-        GetGeocodeAddress.call("Rudi-Dutschke-Str, Berlin")
+        GetGeocodeAddress.new("Rudi-Dutschke-Str, Berlin").call
       end
-      it "raises an exception" do
+      it 'raises an exception' do
         expect { unspecific_address }.to raise_error(/unspecific address/)
       end
     end
     context 'when called with an invalid address' do
-      let(:invalid_address) { GetGeocodeAddress.call("") }
+      let(:invalid_address) { GetGeocodeAddress.new("").call }
       it "raises an exception" do
         expect { invalid_address }.to raise_error(/invalid address/)
       end
