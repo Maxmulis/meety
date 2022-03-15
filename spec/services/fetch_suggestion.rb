591261f7a915c3d0f1f9ad2910d405803f22b957
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-03-15 23:23:00 +0100

    renamed CI/CD workflow to Tests

--- spec/services/fetch_suggestion.rb
+++ spec/services/fetch_suggestion.rb
@@ -4,7 +4,7 @@ RSpec.describe FetchSuggestion, type: :services do
   let(:valid_suggestion) { FetchSuggestion.call(lon: 13.3231312, lat: 52.4605144) }
 
   describe '#call' do
-    context 'when called with valid lon and lat returns a valid place' do
+    context 'when called with valid lon and lat' do
       it 'returns a valid place' do
         expect(valid_suggestion.valid?).to be true
       end
