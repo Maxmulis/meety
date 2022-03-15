4d9e91f2375ca1143db419d6ef4ac7b6af7e1920
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-03-15 20:35:19 +0100

    button to the left

--- app/controllers/meety_controller.rb
+++ app/controllers/meety_controller.rb
@@ -1,32 +1,60 @@
 require 'net/http'
 
-class MeetyController < ApplicationController
+class PagesController < ApplicationController
   rescue_from Net::HTTPBadRequest, with: :bad_request
 
-  def start
-    # json = GetPage.call("")
-    # render json: json, status: :ok
+  def index
+    @place = Place.new
+
+    @categories = {
+      accomodation: "Place to stay or live",
+      activity: "Clubs, community centers",
+      commercial: "Places where one can buy or sell things",
+      catering: "Places of public catering: restaurants, cafes, bars, etc.",
+      education: "Place that provides learning spaces and learning environments",
+      childcare: "Place that provides care of children service while parents are working",
+      entertainment: "Place that where one can spend free time with amusement",
+      leisure: "Places where one can relax and unwind",
+      natural: "Places where one can enjoy nature, explore natural phenomena",
+      parking: "Places where one can park a car",
+      pet: "Places that can be interesting for pet owners",
+      rent: "Places where one can rent things",
+      service: "Places that provide services to the public",
+      tourism: "Places that can be interesting for tourists",
+      sport: "Infrastructure objects related to different sport kinds",
+      public_transport: "Public transport stations and stops"
+    }
+
+    @conditions = {
+      internet_access: "Places that provide an internet access",
+      wheelchair: "Places that are suitable to be used with wheelchairs",
+      dogs: "Places where dogs are allowed",
+      vegetarian: "Places where you can buy or eat vegetarian food",
+      halal: "Places where you can buy or eat halal food",
+      kosher: "Places where you can buy or eat kosher food",
+      organic: "Places where you can buy or eat organic food"
+    }
   end
 
-  def results
+  def show
+    dumbp = %w[internal_access wheelchair dogs vegetarian halal kosher organic]
+
+    @categories = params.select { |_, v| v == "1" }
+    @conditions = params.select { |k| dumbp.include?(k) }
+
     address_one = CGI.escape(params[:address_field_one])
     address_two = CGI.escape(params[:address_field_two])
 
-    @person_one = GeocodeAddress.call(address_one)
-    @person_two = GeocodeAddress.call(address_two)
+    @person_one = GetGeocodeAddress.call(address_one)
+    @person_two = GetGeocodeAddress.call(address_two)
 
     if @person_one.valid? && @person_two.valid?
-      midpoint = GeographicMidpoint.call([@person_one, @person_two])
-      suggestions = [FetchSuggestion.call(midpoint)]
-      @token = FetchTempToken.call
+      @token = GetTempToken.call
       @people_markers = [@person_one, @person_two].map do |place|
         { lat: place.lat, lon: place.lon }
       end
-      @suggestions_markers = suggestions.map do |place|
-        { lat: place.lat, lon: place.lon }
-      end
     else
-      render :results
+      render :index
     end
   end
 
