2a80dc4f3298a4aba44a92c5d6ccb18ed59cd42f
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-03-16 21:26:23 +0100

    WIP

--- app/controllers/meety_controller.rb
+++ app/controllers/meety_controller.rb
@@ -4,41 +4,13 @@ class MeetyController < ApplicationController
   rescue_from Net::HTTPBadRequest, with: :bad_request
 
   def start
-    @categories = {
-      accomodation: "Place to stay or live",
-      activity: "Clubs, community centers",
-      commercial: "Places where one can buy or sell things",
-      catering: "Places of public catering: restaurants, cafes, bars, etc.",
-      education: "Place that provides learning spaces and learning environments",
-      childcare: "Place that provides care of children service while parents are working",
-      entertainment: "Place that where one can spend free time with amusement",
-      leisure: "Places where one can relax and unwind",
-      natural: "Places where one can enjoy nature, explore natural phenomena",
-      parking: "Places where one can park a car",
-      pet: "Places that can be interesting for pet owners",
-      rent: "Places where one can rent things",
-      service: "Places that provide services to the public",
-      tourism: "Places that can be interesting for tourists",
-      sport: "Infrastructure objects related to different sport kinds",
-      public_transport: "Public transport stations and stops"
-    }
-
-    @conditions = {
-      internet_access: "Places that provide an internet access",
-      wheelchair: "Places that are suitable to be used with wheelchairs",
-      dogs: "Places where dogs are allowed",
-      vegetarian: "Places where you can buy or eat vegetarian food",
-      halal: "Places where you can buy or eat halal food",
-      kosher: "Places where you can buy or eat kosher food",
-      organic: "Places where you can buy or eat organic food"
-    }
+    @categories = CATEGORIES
+    @conditions = CONDITIONS
   end
 
   def results
-    dumbp = %w[internal_access wheelchair dogs vegetarian halal kosher organic]
-
-    @categories = params.select { |_, v| v == "1" }
-    @conditions = params.select { |k| dumbp.include?(k) }
+    @categories = MeetyHelper.select_categories(params)
+    @conditions = MeetyHelper.select_conditions(params)
 
     address_one = CGI.escape(params[:address_field_one])
     address_two = CGI.escape(params[:address_field_two])
--- app/services/fetch_suggestion.rb
+++ app/services/fetch_suggestion.rb
@@ -4,14 +4,16 @@ class FetchSuggestion < ApplicationService
   API_KEY = Rails.application.credentials.geoapify
   raise StandardError, "GEOAPIFY_API_KEY unretrievable" if API_KEY.nil?
 
-  def initialize(place)
+  def initialize(place, _category, _condition)
     @place = place
+    # &categories=
+    # &conditions=
   end
 
   def call
-    cat = 'catering.restaurant'
-    filters = "circle:#{@place.lon},#{@place.lat},1000&limit=1"
-    uri = URI("https://api.geoapify.com/v2/places?categories=#{cat}&filter=#{filters}&apiKey=#{API_KEY}")
+    # cat = 'catering.restaurant'
+    filters = "bias=proximity:#{@place.lon},#{@place.lat}&limit=1"
+    uri = URI("https://api.geoapify.com/v2/places?categories=#{@categories}&#{filters}&apiKey=#{API_KEY}")
     res = Net::HTTP.get_response(uri)
     response = JSON.parse(res.body, symbolize_names: true)
     Place.new(response[:features].first[:properties])
