457de5744e3082cbbd24b80d6d3bbf8450ec0d8b
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-03-16 20:34:50 +0100

    cleanup

--- app/controllers/meety_controller.rb
+++ app/controllers/meety_controller.rb
@@ -1,11 +1,9 @@
 require 'net/http'
 
-class PagesController < ApplicationController
+class MeetyController < ApplicationController
   rescue_from Net::HTTPBadRequest, with: :bad_request
 
-  def index
-    @place = Place.new
-
+  def start
     @categories = {
       accomodation: "Place to stay or live",
       activity: "Clubs, community centers",
@@ -36,7 +34,7 @@ class PagesController < ApplicationController
     }
   end
 
-  def show
+  def results
     dumbp = %w[internal_access wheelchair dogs vegetarian halal kosher organic]
 
     @categories = params.select { |_, v| v == "1" }
@@ -45,11 +43,11 @@ class PagesController < ApplicationController
     address_one = CGI.escape(params[:address_field_one])
     address_two = CGI.escape(params[:address_field_two])
 
-    @person_one = GetGeocodeAddress.call(address_one)
-    @person_two = GetGeocodeAddress.call(address_two)
+    @person_one = GeocodeAddress.call(address_one)
+    @person_two = GeocodeAddress.call(address_two)
 
     if @person_one.valid? && @person_two.valid?
-      @token = GetTempToken.call
+      @token = FetchTempToken.call
       @people_markers = [@person_one, @person_two].map do |place|
         { lat: place.lat, lon: place.lon }
       end
