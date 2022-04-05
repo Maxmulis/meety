df1932b9965888309542f390a565d5922fd4c496
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-04-05 19:05:17 +0200

    fixed tests, skipping one

--- spec/services/geographic_midpoint_spec.rb
+++ spec/services/geographic_midpoint_spec.rb
@@ -3,25 +3,24 @@ require 'rails_helper'
 RSpec.describe GeographicMidpoint, type: :services do
   describe '#call' do
     context 'when called with a valid address' do
-      let(:valid_address1) do
-        GeocodeAddress.new("Rudi-Dutschke-Str. 26, Berlin").call
+      let(:valid_place1) do
+        GeocodePlace.new(Place.new(address: "Rudi-Dutschke-Str. 26, Berlin")).call
       end
-      let(:valid_address2) do
-        GeocodeAddress.new("Kochstrasse 9, 10969 Berlin").call
+      let(:valid_place2) do
+        GeocodePlace.new(Place.new(address: "Kochstrasse 9, 10969 Berlin")).call
       end
-
       let(:lat) do
-        GeographicMidpoint.call([valid_address1, valid_address2]).lat
+        GeographicMidpoint.call([valid_place1, valid_place2]).lat
       end
       let(:lon) do
-        GeographicMidpoint.call([valid_address1, valid_address2]).lon
+        GeographicMidpoint.call([valid_place1, valid_place2]).lon
       end
 
       it "returns the geographic midpoint" do
         expect(lon: lon.round, lat: lat.round).to eq(lon: 13, lat: 53)
       end
 
-      let(:place) { GeographicMidpoint.call([valid_address1, valid_address2]) }
+      let(:place) { GeographicMidpoint.call([valid_place1, valid_place2]) }
       it "returns a valid Place" do
         expect(place).to be_a(Place)
       end
