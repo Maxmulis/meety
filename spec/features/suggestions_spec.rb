3b10a3f26dcbb38449e866727a6f542face12796
Lukas Möller <79029266+lmllr@users.noreply.github.com>
2022-04-14 16:56:57 +0200

    fix typo in suggestions_spec

--- spec/features/suggestions_spec.rb
+++ spec/features/suggestions_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 feature "get suggestions" do
-
   scenario "Passing two valid addresses", js: true do
     visit root_path
     fill_in("Your address", with: "Manteuffelstr. 77, 10999 Berlin")
-    fill_in("Your friend's address", with: "Martin-Opitz-Straße 21, 13357 Berlin")
-    click_on("Search for some places"
+    fill_in("Your friend's address",
+            with: "Martin-Opitz-Straße 21, 13357 Berlin")
+    click_on("Search for some places")
 
     expect(page).to have_selector('div.mapboxgl-marker', minimum: 3, wait: 10)
   end
